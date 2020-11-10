fbdc1bcf050a35b7303b1dd306636d5f64670d85
Jonathan DEKHTIAR <contact@jonathandekhtiar.eu>
2020-11-10 15:52:10 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ setup(
         'Topic :: System :: Software Distribution',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules',
